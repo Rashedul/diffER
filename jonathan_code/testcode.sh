536a1be37dfaaab02c619ab14a7b9f2478d4be20
Rashedul Islam <rashedulislam@Rasheduls-MacBook-Pro.local>
2024-11-02 21:25:21 -0700

    js

--- jonathan_code/testcode.sh
+++ jonathan_code/testcode.sh
@@ -1,8 +1,11 @@
+# compare with Rashedul's version
+
 # cll run codes
 cd /projects/blueprint_CLL_dart2/jsteif/
 less /projects/blueprint_CLL_dart2/jsteif/differential_results/tmp_bash_sbatch_H3K27me3_cll_mutated_cll_unmutated_0.03.sh
+bash /projects/blueprint_CLL_dart2/jsteif/scripts/run_opt_two_group_comparision.sh -t cll_mutated -T cll_unmutated -p 0.03 -c chr11 -m H3K27me3 -i /projects/blueprint_CLL_dart2/jsteif/symlinked_final_tables -g ig_status -o /projects/blueprint_CLL_dart2/jsteif/differential_results
 
-# compare number of bins 
+# compare number of bins (okay)
 cd /projects/epigenomics_assembly/jsteif/IHEC/postgresql_tables/ALL/final_tables
 g=$cll/genome_feature/CHROMSIZES_hg38_formatted.txt
 chr22=$cll/genome_feature/CHROMSIZES_hg38_chr22.txt
@@ -19,6 +22,14 @@ bedtools makewindows -g $chr10 -w 50 | wc -l
 
 bedtools makewindows -g $g -w 50 | wc -l
 #64185939 
+
+# compare number of DE bins before merging (does not match)
+cd /projects/blueprint_CLL_dart2/jsteif/differential_results/tmp_bash
+less H3K27me3_cll_mutated_cll_unmutated_chr10_0.03 | wc -l 
+# 16036
+
+less H3K27me3_cll_mutated_cll_unmutated_chr10_0.03 | grep -v "chromosome" | tr ',' "\t" | awk '{print $1 "\t" $2 "\t" $2+50}' | sort -k1,1 -k2,2n | bedtools merge -i stdin -d 100 | wc -l
+# 5097
 
 
 ################################
